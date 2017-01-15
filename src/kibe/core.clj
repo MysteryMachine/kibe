5d0b4032d6c11246bd6afd495eb2dddd9c997bff
Sal Becker <physicsfu@gmail.com>
2017-01-14 23:47:56 -0500

    first draft

---
+++ src/kibe/core.clj
@@ -0,0 +1,69 @@
+(ns kibe.core)
+
+(defprotocol IFailure
+  (fail-unwrap [this]))
+
+(defprotocol ISuccess
+  (success-unwrap [this]))
+
+(defn failure?
+  [i]
+  (satisfies? IFailure i))
+
+(defn success?
+  [i]
+  (satisfies? ISuccess i))
+
+(defn failure
+  [i]
+  (reify IFailure (fail-unwrap [this] i)))
+
+(defn success
+  [i]
+  (reify ISuccess (success-unwrap [this] i)))
+
+(defmacro handle
+  [[val-name i] success-path failure-path]
+  `(let [i# ~i]
+     (if (failure? i#)
+       (let [~val-name (fail-unwrap i#)]
+         ~failure-path)
+       (let [~val-name (success-unwrap i#)]
+         ~success-path))))
+
+(defmacro defh
+  [name doc-string attr-list & forms]
+  (assert (symbol? name) "First form must be a symbol.")
+  (assert (string? doc-string) "Second form must be a doc string.")
+  (assert (and (vector? attr-list) (map symbol? attr-list))
+          "Third form must be a vector of symbols.")
+  `(defn ~name ~doc-string ~attr-list
+     (try
+       (let [retval# (do ~@forms)]
+         (assert
+          (or (failure? retval#) (success? retval#))
+          (str "Function " ~name " did not a return an explict"
+               " success or failure value."))
+         retval#)
+       (catch Exception e#
+         (failure {:exception e#})))))
+
+(defmacro cond?>
+  [i & forms]
+  (if (seq forms)
+    (let [[[f & r] & forms] forms]
+      `(handle
+        [r# (~f ~i ~@r)]
+        (cond?> r# ~@forms)
+        (failure r#)))
+    `(success ~i)))
+
+(defmacro cond?>>
+  [i & forms]
+  (if (seq forms)
+    (let [[[f & r] & forms] forms]
+      `(handle
+        [r# (~f ~@r ~i)]
+        (cond?>> r# ~@forms)
+        (failure r#)))
+    `(success ~i)))
---
+++ test/kibe/core_test.clj
@@ -0,0 +1,128 @@
+(ns kibe.core-test
+  (:require [clojure.test :refer :all]
+            [kibe.core :refer :all]))
+
+(deftest failure-test
+  (let [target (failure true)]
+    (is (satisfies? IFailure target))
+    (is (true? (fail-unwrap target)))))
+
+(deftest success-test
+  (let [target (success true)]
+    (is (satisfies? ISuccess target))
+    (is (true? (success-unwrap target)))))
+
+(deftest success?-test
+  (is (success? (success true))))
+
+(deftest failure?-test
+  (is (failure? (failure true))))
+
+(deftest handle-test
+  (testing "failure"
+    (handle
+     [target (failure true)]
+     (throw (Exception. "Error, should've gone down the other path"))
+     (is (true? target))))
+  (testing "success"
+    (handle
+     [target (success true)]
+     (is (true? target))
+     (throw (Exception. "Error, should've gone down the other path")))))
+
+(defh test-1
+  "A sample handler"
+  [a b]
+  (if (> a b)
+    (success (- a b))
+    (failure {:error true})))
+
+(defh test-2
+  "A sample handler"
+  [a b]
+  (success (/ a b)))
+
+(deftest defh-test
+  (testing "test-1"
+    (handle
+     [result (test-1 2 1)]
+     (is (= 1 result))
+     (throw (Exception. "Error, should've gone down the other path")))
+    (handle
+     [result (test-1 1 1)]
+     (throw (Exception. "Error, should've gone down the other path"))
+     (is (= {:error true} result))))
+  (testing "test-2"
+    (handle
+     [result (test-2 2 1)]
+     (is (= 2 result))
+     (throw (Exception. "Error, should've gone down the other path")))
+    (handle
+     [result (test-2 1 0)]
+     (throw (Exception. "Error, should've gone down the other path"))
+     (is (:exception result)))))
+
+(defh test-3
+  "A sample handler"
+  [a b c]
+  (success (+ (* a a) (- b c))))
+
+(defh test-4
+  "A sample handler"
+  [a b c d]
+  (cond?>
+   a
+   (test-1 b)
+   (test-3 c d)
+   (test-1 b)))
+
+(defh test-5
+  "A sample handler"
+  [a b c d]
+  (cond?>>
+   a
+   (test-1 b)
+   (test-3 c d)
+   (test-1 b)))
+
+(deftest cond?>-test
+  (handle
+   [result (test-4 25 5 2 3)]
+   (is (= 394 result))
+   (throw (Exception. "Error, should've gone down the other path")))
+
+  (handle
+   [result (test-4 25 5 2 10)]
+   (is (= 387 result))
+   (throw (Exception. "Error, should've gone down the other path")))
+
+  (handle
+   [result (test-4 0 5 2 10)]
+   (throw (Exception. "Error, should've gone down the other path"))
+   (is (= {:error true} result)))
+
+  (handle
+   [result (test-4 6 5 0 0)]
+   (throw (Exception. "Error, should've gone down the other path"))
+   (is (= {:error true} result))))
+
+(deftest cond?>>-test
+  (handle
+   [result (test-5 5 25 2 3)]
+   (is (= 38 result))
+   (throw (Exception. "Error, should've gone down the other path")))
+
+  (handle
+   [result (test-5 5 25 2 10)]
+   (is (= 31 result))
+   (throw (Exception. "Error, should've gone down the other path")))
+
+  (handle
+   [result (test-5 5 0 2 10)]
+   (throw (Exception. "Error, should've gone down the other path"))
+   (is (= {:error true} result)))
+
+  (handle
+   [result (test-5 5 6 0 7)]
+   (throw (Exception. "Error, should've gone down the other path"))
+   (is (= {:error true} result))))
